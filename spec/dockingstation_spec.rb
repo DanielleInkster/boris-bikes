5aa9581a9361edcb00d415b78ac59149485b4401
DanielleInkster <danielle.inkster@gmail.com>
2019-09-17 16:09:16 +0100

    RSPEC for Instance of Bike/Respond to Working?

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,4 +3,9 @@ require 'dockingstation'
 describe DockingStation do
 
 it { is_expected.to respond_to(:release_bike) }
+
+it { expect(DockingStation.new.release_bike.class).to eq Bike }
+
+it { expect(DockingStation.new.release_bike).to respond_to (:working?)}
+
 end
