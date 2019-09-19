5ef75b138bef6cdd4fdd37076fd637ce71e532df
Duncan Skinner <duncanskinner94@gmail.com>
2019-09-19 16:02:22 +0100

    changing capacity to DEFAULT_CAPACITY contant

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,43 +2,43 @@ require 'dockingstation'
 require 'bike'
 
 describe DockingStation do
+  DEFAULT_CAPACITY = 20
+  it { is_expected.to respond_to(:release_bike) }
 
-it { is_expected.to respond_to(:release_bike) }
+  it { is_expected.to respond_to(:dock).with(1).argument }
 
-it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike) }
 
-it { is_expected.to respond_to(:bike) }
-
-describe '#working?' do
-  it 'tests bike is working' do
-   bike = Bike.new
-   expect(bike.working?).to eq true
+  describe '#working?' do
+    it 'tests bike is working' do
+     bike = Bike.new
+     expect(bike.working?).to eq true
+    end
   end
-end
 
-describe '#dock' do
-  it 'returns docked bikes' do
-   bike = Bike.new
-   subject.dock(bike)
-   expect(subject.bike).to eq @bikes
- end
+  describe '#dock' do
+    it 'returns docked bikes' do
+     bike = Bike.new
+     subject.dock(bike)
+     expect(subject.bike).to eq @bikes
+   end
 
- it 'raises an error for full capacity' do
-  20.times { subject.dock Bike.new }
-  expect { subject.dock Bike.new }.to raise_error('Docking station full')
+   it 'raises an error for full capacity' do
+    DEFAULT_CAPACITY.times { subject.dock Bike.new }
+    expect { subject.dock Bike.new }.to raise_error('Docking station full')
+    end
   end
-end
 
- describe '#release_bike' do
-  it 'releases a bike' do
-   bike = Bike.new
-   subject.dock(bike)
-   expect(subject.release_bike).to eq bike
- end
+   describe '#release_bike' do
+    it 'releases a bike' do
+     bike = Bike.new
+     subject.dock(bike)
+     expect(subject.release_bike).to eq bike
+   end
 
- it 'raises an error for no bikes' do
-   expect {subject.release_bike}.to raise_error('No bikes available')
+   it 'raises an error for no bikes' do
+     expect {subject.release_bike}.to raise_error('No bikes available')
+    end
   end
-end
 
 end
